8612ebd064bca2d2d091fc7ae55b5be718913b45
brandon <brandon1324@hotmail.com>
2018-12-23 22:18:10 +0100

    Rendimiento : ahora no se envian mensajes al que envia

--- sem04/sem04.js
+++ sem04/sem04.js
@@ -1,26 +1,38 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var usuarios = []
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    console.log('Alguien se ha conectado');
-    socket.username = 'anonimo';
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        console.log("usuario desconectado");
+        io.emit('desconectado',{usuario : socket.username});
     });
     socket.on('mensaje', function(datos){
-        console.log('message: ' + datos);
-        io.emit('mensaje', {usuario : socket.username, msj : datos.msj});
+
+        if(datos.usuario !== socket.username){
+            console.log('message: ' + socket.username);
+            io.emit('mensaje', {usuario : socket.username, msj : datos.msj});
+        }
+
     });
     socket.on('cambiar_nombre',function(datos){
         console.log('se ha cambiado el nombre a ${datos.usuario}');
         socket.username = datos.usuario;
-    })
+    });
+    socket.on('conectado',function () {
+        io.emit('conectado',{usuario: socket.username});
+        console.log(socket.username + ' se ha conectado');
+    });
+    socket.on('anyadir_usuario',function(datos){
+        console.log('se ha cambiado el nombre a ${datos.usuario}');
+        socket.username = datos.usuario;
+        usuarios.push(datos.usuario);
+    });
 });
 
 http.listen(3000, function(){
